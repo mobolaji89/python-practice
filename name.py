ef7e7baf54e859da2d00112ab899e1cbdefed70d
Mobolaji David <davidmo@colorado.edu>
2023-08-07 15:03:53 -0700

    lower and upper methods

--- name.py
+++ name.py
@@ -1,4 +1,8 @@
 # variables
 
 name = "stephen curry"
-print(name.title()) #the title method changes each word to title case
+print(name.title()) # the title() method changes each word to title case
+
+name = "Stephen Curry"
+print(name.upper()) # the upper() method changes a string to all uppercase
+print(name.lower()) # the lower() method changes a string to all lowercase. Useful for storing data
